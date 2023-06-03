f3dda408552d0cc390d1e4addaf33a603d622c2a
robertopracticaspg <93863224+robertopracticaspg@users.noreply.github.com>
2023-06-03 11:20:50 +0200

    Update HotelController.java
    añadido de swagger

--- Ej05_Hotel/src/main/java/com/curso/controller/HotelController.java
+++ Ej05_Hotel/src/main/java/com/curso/controller/HotelController.java
@@ -9,7 +9,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.curso.model.Hotel;
+
 import com.curso.service.HotelService;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @RestController
 public class HotelController {
@@ -22,11 +27,20 @@ public class HotelController {
 	 * 
 	 * @return La lista de hoteles.
 	 */
+	// endpoint
+	// http://localhost:8080/hoteles GET
+	@ApiOperation(value = "Devuelve la lista con todos los hoteles en formato JSON", notes = "Si necesito explicar con más detalle lo hago aqui")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "OK. El recurso se obtiene correctamente", response = Hotel.class),
+			@ApiResponse(code = 404, message = "Método no permitido."),
+			@ApiResponse(code = 500, message = "Error inesperado del sistema") })
 	@GetMapping(value = "hoteles", produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Hotel> hotel() {
 		return service.hotel();
 	}
 
+	// endpoint
+	// http://localhost:8080/hoteles/Melia GET
 	/**
 	 * Obtiene los datos de un hotel por su nombre.
 	 * 
